7db09b3e0f1c1d9df9d3fadff11e4a7999de178b
Mahmudi <136174686+Mahmudi14@users.noreply.github.com>
2023-12-29 13:18:03 +0800

    NOMOR 5

---
+++ NOMOR_5/GUI_VisualiisasiAlgoritma/src/test/test01.java
@@ -0,0 +1,15 @@
+package test;
+
+import javax.swing.JFrame;
+
+public class test01 {
+    public static void main(String[] args) {
+        JFrame fr = new JFrame();
+        Canvas canvas = new Canvas();
+        fr.getContentPane().add(canvas);
+        fr.setSize(500,500);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fr.setVisible(true);
+        
+    }
+}
